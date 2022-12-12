7010bb1bff48c7a08471fdf55e69d9fb0252309e
Adam Burke <adamburkemail@gmail.com>
2022-12-12 12:14:54 +1000

    Make mnode incremental available to command line

--- src/main/haskell/Main.hs
+++ src/main/haskell/Main.hs
@@ -33,7 +33,7 @@ data Model = Stochastic | ControlFlow
 data PTreeFormat = PTree | LaTeX
         deriving (Show,Eq)
 
-data Impl = Incr | Batch | MNode
+data Impl = Incr | Binary | MNode
         deriving (Show,Eq)
 
 toothpasteArgs = ToothpasteArgs{
@@ -46,7 +46,7 @@ toothpasteArgs = ToothpasteArgs{
     ptreefile = "" &= help "Output PPTree file",
     ptreeformat = "ptree" 
             &= help "Output PPT format. Valid values ptree or latex",
-    impl      = "mnode" &= help "Discovery algo. Valid values batch, incr, or mnode. Default mnode.",
+    impl      = "mnode" &= help "Discovery algo. Valid values binary, incr, or mnode. Default mnode.",
     traceprobfile = ""
             &= help "Output trace probabilities to this file",
     noise = def
@@ -70,7 +70,7 @@ modelSelector mstr | mstr == "stoch" = Stochastic
 
 implSelector :: String -> Impl
 implSelector istr | istr == "incr"  = Incr
-                  | istr == "batch" = Batch
+                  | istr == "binary" = Binary
                   | istr == "mnode" = MNode
                   | otherwise       = MNode
 
@@ -90,16 +90,9 @@ pptreeIntToStrB (Binpaste.Node1 op x r n) m =
 pptreeIntToStrB (Binpaste.Node2 op x y n) m = 
     Binpaste.Node2 op (pptreeIntToStrB x m) (pptreeIntToStrB y m) n
 
-ptreeOnIntBatchB :: ToothpasteArgs -> String -> Binpaste.PPTree String
-ptreeOnIntBatchB tpargs rawlog = 
+ptreeOnIntBinary :: ToothpasteArgs -> String -> Binpaste.PPTree String
+ptreeOnIntBinary tpargs rawlog = 
                     pptreeIntToStrB (Binpaste.discoverGen intlog) m
-        where   strlog     = (parseSelector $ logformat tpargs) rawlog
-                (intlog,m) = logIndex strlog
-
-
-ptreeOnIntInc :: ToothpasteArgs -> String -> Binpaste.PPTree String
-ptreeOnIntInc tpargs rawlog   = 
-                    pptreeIntToStrB (Binpaste.batchIncDiscover intlog) m
         where   strlog     = (parseSelector $ logformat tpargs) rawlog
                 (intlog,m) = logIndex strlog
 
@@ -122,6 +115,12 @@ pptreeIntToStrList ptl m = map (`pptreeIntToStr` m) ptl
 ptreeOnIntBatch :: ToothpasteArgs -> String -> ProbProcessTree.PPTree String 
 ptreeOnIntBatch tpargs rawlog = 
                     pptreeIntToStr (TPMine.discoverGen intlog) m
+        where   strlog     = (parseSelector $ logformat tpargs) rawlog
+                (intlog,m) = logIndex strlog
+
+ptreeOnIntInc :: ToothpasteArgs -> String -> ProbProcessTree.PPTree String
+ptreeOnIntInc tpargs rawlog   = 
+                    pptreeIntToStr (TPMine.batchIncDiscover intlog) m
         where   strlog     = (parseSelector $ logformat tpargs) rawlog
                 (intlog,m) = logIndex strlog
 
@@ -157,12 +156,12 @@ traceProbForLog strlog pptm
 
 mine :: ToothpasteArgs -> String -> (String, String)
 mine tpargs logtext
-    | model == Stochastic && algo == Batch = 
+    | model == Stochastic && algo == Binary = 
         (Binpaste.formatPPTree pptb, 
          weightedNetToString (Binpaste.translate pptb) "spn" )
     | model == Stochastic && algo == Incr = 
-         (Binpaste.formatPPTree ppti, 
-          weightedNetToString (Binpaste.translate ppti) "spn" )
+         (pptformatter pptm,
+          weightedNetToString (TPMine.translate ppti) "spn" )
     | model == Stochastic && algo == MNode && nse <= 0.0 = 
          (pptformatter pptm, 
           weightedNetToString (TPMine.translate pptm) "spn" ) 
@@ -176,7 +175,7 @@ mine tpargs logtext
           algo   = implSelector  $ impl tpargs
           pptf   = ptreeformatSelector $ ptreeformat tpargs
           nse    = noise tpargs
-          pptb   = ptreeOnIntBatchB tpargs logtext
+          pptb   = ptreeOnIntBinary tpargs logtext
           ppti   = ptreeOnIntInc    tpargs logtext
           pptm   = ptreeOnIntBatch  tpargs logtext
           pptn   = ptreeOnIntNoise  tpargs logtext
